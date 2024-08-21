a50f0f40a265c5d81f0ac1e1bef177a29e8d1b92
Gurnam <lohiagurunam11@gmail.com>
2024-08-21 21:30:33 +0530

    Update app.py

--- text23/app.py
+++ text23/app.py
@@ -243,6 +243,6 @@ async def process_image(
     else:
         return {"Authentication":"Failed"}
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=7860)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host="0.0.0.0", port=7860)
