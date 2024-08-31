18c5e6650c56ebd924412d139f29f6b7d22d91a0
Gurnam <lohiagurunam11@gmail.com>
2024-08-31 14:57:07 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,21 @@ from PIL import Image
 from functools import partial
 import io
 import uvicorn
-
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (adjust as needed)
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+app.add_middleware(HTTPSRedirectMiddleware)
+
 
 client = Client("vibs08/flash-sd3-new", hf_token="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")
 
@@ -27,7 +39,7 @@ bedrock_runtime = boto3.client(service_name='bedrock-runtime', aws_access_key_id
 
 
 def gen_pos_prompt(text):
-  instruction = f'''Your task is to create a positive prompt for image generation.
+  instruction = f'''Your task is to create a positive prompt for image generation under 28 words.
 
     Objective: Generate images that prioritize structural integrity and accurate shapes. The focus should be on the correct form and basic contours of objects, with minimal concern for colors.
     
@@ -60,8 +72,6 @@ def gen_pos_prompt(text):
     Additional Details: Keep additional details minimal.
 
 
-    ### MAXIMUM OUTPUT LENGTH SHOULD BE UNDER 30 WORDS ###
-
     Input: {text}
     Positive Prompt: 
     '''
@@ -77,7 +87,7 @@ def text2img(promptt):
     result = client.predict(
         prompt=promptt,  
         seed=0,
-        randomize_seed=False,
+        randomize_seed=True,
         guidance_scale=1,
         num_inference_steps=4,
         negative_prompt="deformed, distorted, disfigured, poorly drawn, bad anatomy, wrong anatomy, extra limb, missing limb, floating limbs, mutated hands and fingers, disconnected limbs, mutation, mutated, ugly, disgusting, blurry, amputation, NSFW, bad text",
@@ -115,35 +125,61 @@ def aws_bedrock(pos_prompt,output_path='output_image.png'):
   return output_path
 
 
-def three_d(prompt, seed, fr, mc, auth, use_aws, text=None):
+def stability(image):
+  url = "https://vibs08-stable-fast-3d.hf.space/process-image/?foreground_ratio=0.85"
+
+
+  files = {
+      "file": (image, open(image, "rb"), "image/png"),
+  }
+
+  response = requests.post(url, files=files)
+
+  return response.json()
+
+
+def three_d(prompt, seed, fr, mc, auth, text=None):
     new_prompt = gen_pos_prompt(prompt)
 
     results = {}
-
+    
     # Try running text2img
     try:
-        text2img_file_path = text2img(new_prompt)
-        with open(text2img_file_path, 'rb') as image_file:
-            files = {
-                'file': (text2img_file_path, image_file, 'image/png')
+      text2img_file_path = text2img(new_prompt)
+      with open(text2img_file_path, 'rb') as image_file:
+        files = {
+            'file': (text2img_file_path, image_file, 'image/png')
             }
-
-            headers = {
+        headers = {
                 'accept': 'application/json'
-            }
-
-            response = requests.post(url, headers=headers, files=files, data=payload)
-            result = response.json()
-            results["text2img_img_path"] = result.get("img_path")
-            results["text2img_obj_path"] = result.get("obj_path")
-            results["text2img_glb_path"] = result.get("glb_path")
+                }
+        payload = {
+                    'seed': seed,
+                    'enhance_image': False,
+                    'do_remove_background': True,
+                    'foreground_ratio': fr,
+                    'mc_resolution': mc,
+                    'auth': auth,
+                    'text_prompt': text
+                    }
+        response = requests.post(url, headers=headers, files=files,data=payload)
+        result = response.json()
+        results["text2img_img_path"] = result.get("img_path")
+        results["text2img_obj_path"] = result.get("obj_path")
+        results["text2img_glb_path"] = result.get("glb_path")
     except Exception as e:
-        results["text2img_error"] = str(e)
+      results["text2img_error"] = str(e)
+    try:
+      text2img_file_path = text2img(new_prompt)
+      new_response = stability(text2img_file_path)
+      results["stabilityai_glb_path"] = new_response.get("glb2_path")
+    except Exception as e:
+      results["stability__error"] = str(e)
 
     # Try running aws_bedrock
     try:
-        aws_bedrock_file_path = aws_bedrock(new_prompt)
-        with open(aws_bedrock_file_path, 'rb') as image_file:
+      aws_bedrock_file_path = aws_bedrock(new_prompt)
+      with open(aws_bedrock_file_path, 'rb') as image_file:
             files = {
                 'file': (aws_bedrock_file_path, image_file, 'image/png')
             }
@@ -151,17 +187,45 @@ def three_d(prompt, seed, fr, mc, auth, use_aws, text=None):
             headers = {
                 'accept': 'application/json'
             }
-
-            response = requests.post(url, headers=headers, files=files, data=payload)
+            payload = {
+                'seed': seed,
+                'enhance_image': False,
+                'do_remove_background': True,
+                'foreground_ratio': fr,
+                'mc_resolution': mc,
+                'auth': auth,
+                'text_prompt': text
+                }
+            response = requests.post(url, headers=headers, files=files,data=payload)
             result = response.json()
             results["aws_bedrock_img_path"] = result.get("img_path")
             results["aws_bedrock_obj_path"] = result.get("obj_path")
             results["aws_bedrock_glb_path"] = result.get("glb_path")
     except Exception as e:
-        results["aws_bedrock_error"] = str(e)
+      results["aws_bedrock_error"] = str(e)
 
-    return results
+    try:
+      aws_bedrock_file_path = aws_bedrock(new_prompt)
+      new_response = stability(aws_bedrock_file_path)
+      results["aws_stability_glb_path"] = new_response.get("glb2_path")
+    except Exception as e:
+      results["aws_stability_error"] = str(e)
 
+    return results   
+
+@app.post("/process_text/")
+async def process_text(
+    text_prompt: str = Form(...),
+    seed: int = Form(...),
+    foreground_ratio: float = Form(...),
+    mc_resolution: int = Form(...),
+    auth: str = Form(...)
+    #use_aws: bool = Form(...)
+):
+    if auth == "userName":
+        return three_d(text_prompt, seed, foreground_ratio, mc_resolution, auth)
+    else:
+        return {"Authentication": "Failed"}
 
 #if __name__ == "__main__":
 #    import uvicorn
